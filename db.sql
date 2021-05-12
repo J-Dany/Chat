aba2a11456049a0fbf65ea6cf745d36647db3a11
J-Dany <jdanya@outlook.com>
2021-05-12 09:45:30 +0200

    Aggiornato script database

--- db.sql
+++ db.sql
@@ -1,45 +1,44 @@
-DROP DATABASE IF EXISTS chat;
-CREATE DATABASE chat CHARACTER SET utf8;
+DROP DATABASE IF EXISTS `chat`;
+CREATE DATABASE `chat`;
+USE `chat`;
 
-USE chat;
-
-CREATE TABLE users(
-	username VARCHAR(64) NOT NULL PRIMARY KEY,
+CREATE TABLE `users`(
+	`username` VARCHAR(64) NOT NULL PRIMARY KEY,
 	`password` VARCHAR(255) NOT NULL,
 	`name` VARCHAR(64) NOT NULL,
-	surname VARCHAR(64) NOT NULL,
-	email VARCHAR(64) NOT NULL
+	`surname` VARCHAR(64) NOT NULL,
+	`email` VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE friends(
-	user1 VARCHAR(64) NOT NULL,
-	user2 VARCHAR(64) NOT NULL,
-	CONSTRAINT pk_Friends PRIMARY KEY(user1, user2),
-	CONSTRAINT fk_friendsUsers1 FOREIGN KEY (user1) REFERENCES users(username),
-	CONSTRAINT fk_friendsUsers2 FOREIGN KEY (user2) REFERENCES users(username)
+CREATE TABLE `friends`(
+	`user1` VARCHAR(64) NOT NULL,
+	`user2` VARCHAR(64) NOT NULL,
+	CONSTRAINT `pk_Friends` PRIMARY KEY(`user1`, `user2`),
+	CONSTRAINT `fk_friendsUsers1` FOREIGN KEY (`user1`) REFERENCES `users`(`username`),
+	CONSTRAINT `fk_friendsUsers2` FOREIGN KEY (`user2`) REFERENCES `users`(`username`)
 );
 
-CREATE TABLE groups(
-	id_group INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	group_name VARCHAR(255) NOT NULL
+CREATE TABLE `groups`(
+	`id_group` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`group_name` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE messages(
-	id_message INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`id_message` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`data` DATETIME NOT NULL,
-	message VARCHAR(512) NOT NULL,
-	id_group INT,
-	sender VARCHAR(64) NOT NULL,
-	addresse VARCHAR(64),
-	CONSTRAINT fk_messagesGroups FOREIGN KEY (id_group) REFERENCES groups(id_group),
-	CONSTRAINT fk_messagesUsersSender FOREIGN KEY (sender) REFERENCES users(username),
-	CONSTRAINT fk_messagesUsersAddresse FOREIGN KEY (addresse) REFERENCES users(username)
+	`message` VARCHAR(512) NOT NULL,
+	`id_group` INT,
+	`sender` VARCHAR(64) NOT NULL,
+	`addresse` VARCHAR(64),
+	CONSTRAINT `fk_messagesGroups` FOREIGN KEY (`id_group`) REFERENCES `groups`(`id_group`),
+	CONSTRAINT `fk_messagesUsersSender` FOREIGN KEY (`sender`) REFERENCES `users`(`username`),
+	CONSTRAINT `fk_messagesUsersAddresse` FOREIGN KEY (addresse) REFERENCES users(username)
 );
 
-CREATE TABLE partecipants(
-	id_group INT NOT NULL,
-	username VARCHAR(64) NOT NULL,
-	CONSTRAINT pk_Partecipants PRIMARY KEY(id_group, username),
-	CONSTRAINT fk_partecipantsUsers FOREIGN KEY (username) REFERENCES users(username),
-	CONSTRAINT fk_partecipantsGroups FOREIGN KEY (id_group) REFERENCES groups(id_group)
+CREATE TABLE `partecipants`(
+	`id_group` INT NOT NULL,
+	`username` VARCHAR(64) NOT NULL,
+	CONSTRAINT `pk_Partecipants` PRIMARY KEY(`id_group`, `username`),
+	CONSTRAINT `fk_partecipantsUsers` FOREIGN KEY (`username`) REFERENCES `users`(`username`),
+	CONSTRAINT `fk_partecipantsGroups` FOREIGN KEY (`id_group`) REFERENCES `groups`(`id_group`)
 );
